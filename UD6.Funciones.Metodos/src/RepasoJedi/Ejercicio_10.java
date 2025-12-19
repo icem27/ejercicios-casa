054d08b7fb8a45e89a04f32a0adfba412eb5edc7
icem27 <icem27@educa.madrid.org>
2025-12-19 14:00:34 +0100

    actualizado

--- UD6.Funciones.Metodos/src/RepasoJedi/Ejercicio_10.java
+++ UD6.Funciones.Metodos/src/RepasoJedi/Ejercicio_10.java
@@ -16,17 +16,14 @@ public class Ejercicio_10 {
 	 */
 
 	public static void main(String[] args) {
-		double x= 5;
-		double y= 6;
-		double z=8;
+		double x= 3;
+		double y= 4;
+		double z=5;
 		
 		pitagoras(x, y, z);
 	}
 	public static void pitagoras(double x, double y, double z) {
-		double k = Math.pow(y, 2);
-		double o = Math.pow(y, 2);
-		double p = Math.pow(z, 2);
-		if((k+o)==p) {
+		if(Math.pow(x, 2) + Math.pow(y, 2) == Math.pow(z, 2)) {
 			System.out.println("Es rectángulo");
 		} else {
 			System.out.println("No es rectángulo");
