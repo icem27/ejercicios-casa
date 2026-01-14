e86233635568307c10dae51b060c8e1718e5a1a9
icem27 <icem27@educa.madrid.org>
2026-01-14 20:57:18 +0100

    actualización

--- UD6.Funciones.Metodos/src/Examen/Ejercicio6.java
+++ UD6.Funciones.Metodos/src/Examen/Ejercicio6.java
@@ -3,14 +3,20 @@ package Examen;
 public class Ejercicio6 {
 
 	public static void main(String[] args) {
-		repiteMensaje("FUTURO", -1);
-
+		String mensaje= repiteMensaje("FUTURO", 3);
+		System.out.println(mensaje);
 	}
-	public static void repiteMensaje(String mensaje, int repetir) {
-		if(repetir<=0) System.out.print("");
-		for(int i=0;i<repetir;i++) {
-		System.out.print(mensaje + " ");
-		}
+	public static String repiteMensaje(String mensaje, int repetir) {
+		if(repetir<=0) return "";
+		
+		return mensaje+" "+repiteMensaje(mensaje, repetir-1);
+		
+		
+		
+//		for(int i=0;i<repetir;i++) {
+//		System.out.print(mensaje + " ");
+//		}
+		
 	}
 	
 }
