472a95af0426be992cc602617393d645dd8c6393
icem27 <icem27@educa.madrid.org>
2026-01-28 18:24:04 +0100

    actualización de jugador

--- UD7.POO/src/Arrays/Jugador_principal.java
+++ UD7.POO/src/Arrays/Jugador_principal.java
@@ -6,16 +6,18 @@ public class Jugador_principal {
 		Jugador[] jugadores = new Jugador[5];
 		jugadores[0] = new Jugador("Ismael", 15);
 		jugadores[1] = new Jugador("Chaniel", 5);
-		jugadores[2] = new Jugador("Yukai", 2);
-		jugadores[3] = new Jugador("Xavi", 0);
+		jugadores[2] = new Jugador("Yukai", 20);
+		jugadores[3] = new Jugador("Xavi", 19);
 		jugadores[4] = new Jugador("Ruth", 4);
 		
-		for(int i=0;i<jugadores.length;i++) {
-			int max=Math.max(jugadores[0].getGolesMarcados(), -1);
-			if(max<=jugadores[i].getGolesMarcados()) {
-				System.out.println("El máximo goleador es "  + jugadores[i].getNombre() + " con " + jugadores[i].getGolesMarcados() + " goles.");
-			}
-		}
+		Jugador max = jugadores[0];
+        for (int i=0;i<jugadores.length;i++) {
+            if (jugadores[i].getGolesMarcados() > max.getGolesMarcados()) {
+                max = jugadores[i];
+            }
+        }
+        System.out.println(max.getNombre() + " con " + max.getGolesMarcados() + " goles.");
+
 
 	}
 
