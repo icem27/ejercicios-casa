9a2dd1f37effa482be940327b4ffb28cff13a894
icem27 <icem27@educa.madrid.org>
2026-01-28 21:04:20 +0100

    nuevo ejercicio

---
+++ UD7.POO/src/Arrays/Habitacion.java
@@ -0,0 +1,44 @@
+package Arrays;
+
+public class Habitacion {
+	/*
+	 * Crea una clase Habitacion con los atributos numero y ocupada (boolean). En el
+	 * main, crea un array con 10 habitaciones, numeradas a partir del 100, marca
+	 * aleatoriamente algunas como ocupadas y muestra cuántas están disponibles.
+	 */
+	
+	private int numero;
+	private boolean ocupada;
+	
+	public Habitacion(int numero, boolean ocupada) {
+		this.numero = numero;
+		this.ocupada = ocupada;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public boolean isOcupada() {
+		return ocupada;
+	}
+
+	public void setOcupada(boolean ocupada) {
+		this.ocupada = ocupada;
+	}
+
+	@Override
+	public String toString() {
+		return "Habitacion [numero=" + numero + ", ocupada=" + ocupada + "]";
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ UD7.POO/src/Arrays/Habitacion_principal.java
@@ -0,0 +1,31 @@
+package Arrays;
+
+public class Habitacion_principal {
+
+	static Habitacion[] hotel = new Habitacion[10];
+
+	public static void main(String[] args) {
+		iniciarlizarArray();
+		
+		for(Habitacion habitacion:hotel) {
+			if(habitacion.isOcupada()) {
+				System.out.println(habitacion);
+			}
+		}
+	}
+	
+	public static void iniciarlizarArray() {
+		int num=100;
+		boolean ocupada=false;
+		
+		for(int i=0;i<hotel.length;i++) {
+			if(num%2==0) {
+				ocupada=true;
+			}
+			hotel[i] = new Habitacion(num, ocupada);
+			ocupada=false;
+			num++;
+		}
+	}
+
+}
