f0942cc274daa4cdc850a0f096f958008eb8d3a7
icem27 <icem27@educa.madrid.org>
2026-01-29 16:41:38 +0100

    actualiza

--- UD7.POO/src/ejemplos/ClaseConMain.java
+++ UD7.POO/src/ejemplos/ClaseConMain.java
@@ -28,19 +28,32 @@ public class ClaseConMain {
 
 	@Override
 	public String toString() {
-		return "Nombre " + nombre + " y tiene edad " + edad;
+		return nombre + " tiene " + edad + " años";
 	}
 
 	public static void main(String[] args) {
-		ClaseConMain[] clase= new ClaseConMain[5];
+		ClaseConMain[] clase= new ClaseConMain[8];
+		String[] nombre = {"Ismael", "Antonio", "Carlos", "Patricia", "Sofia", "Sara", "Carla", "Francisco"};
 		
 		for(int i=0;i<clase.length;i++) {
-			clase[i]=new ClaseConMain("Ismael", 12+(i*2));
+			int edad=(int)(Math.random()*99);
+			clase[i]=new ClaseConMain(nombre[i], edad);
 		}
 		
+		ClaseConMain masViejo=clase[0];
+		ClaseConMain masJoven=clase[0];
 		for(ClaseConMain x:clase) {
 			System.out.println(x);
+			if(masViejo.getEdad()<x.getEdad()) {
+				masViejo=x;
+			}
+			if(masJoven.getEdad()>x.getEdad()) {
+				masJoven=x;
+			}
 		}
+
+		System.out.println("El mas viejo es: " + masViejo);
+		System.out.println("El mas joven es: " + masJoven);
 		
 	}
 
