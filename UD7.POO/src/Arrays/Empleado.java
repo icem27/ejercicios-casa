45e994fed0ee0d762773c9f76e65207890de6af0
icem27 <icem27@educa.madrid.org>
2026-01-29 10:02:12 +0100

    Nuevo ejercicio de empleado

---
+++ UD7.POO/src/Arrays/Empleado.java
@@ -0,0 +1,46 @@
+package Arrays;
+
+public class Empleado {
+	/*
+	 * Crea una clase Empleado con atributos nombre, sueldo y departamento. Luego,
+	 * en el main, crea un array con 5 empleados y cuenta cuántos pertenecen al
+	 * departamento de "Ventas".
+	 */
+	
+	private String nombre;
+	private double sueldo;
+	private String departamento;
+	
+	public Empleado(String nombre, double sueldo, String departamento) {
+		this.nombre = nombre;
+		this.sueldo = sueldo;
+		this.departamento = departamento;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getSueldo() {
+		return sueldo;
+	}
+	public void setSueldo(double sueldo) {
+		this.sueldo = sueldo;
+	}
+	public String getDepartamento() {
+		return departamento;
+	}
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+
+	@Override
+	public String toString() {
+		return "Empleado [nombre=" + nombre + ", sueldo=" + sueldo + ", departamento=" + departamento + "]";
+	}
+	
+	
+	
+}
---
+++ UD7.POO/src/Arrays/Empleado_principal.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class Empleado_principal {
+
+	public static void main(String[] args) {
+		Empleado[] empresa = new Empleado[5];
+		empresa[0] = new Empleado("Ismael", 2958.4, "Ventas"); 
+		empresa[1] = new Empleado("Chaniel", 958.9, "Informatica"); 
+		empresa[2] = new Empleado("Yukai", 1205.9, "Compras"); 
+		empresa[3] = new Empleado("Ruth", 1564, "Ventas"); 
+		empresa[4] = new Empleado("Xavi", 1325, "Informatica"); 
+		String buscador="Ventas";
+		int contador=0;
+		for(Empleado masEmpleados:empresa) {
+			if(masEmpleados.getDepartamento().equalsIgnoreCase(buscador)) {
+				contador++;
+			}
+		}
+		
+		System.out.println("En el departamento " + buscador + " hay " + contador + " empleados");
+
+	}
+
+}
