e64bb49831bcbf46ead5ff902bb5ea2091c0bdcd
icem27 <icem27@educa.madrid.org>
2026-02-02 09:54:38 +0100

    nuevo ejercicio

---
+++ UD7.POO/src/RepasoFinal/Atleta.java
@@ -0,0 +1,50 @@
+package RepasoFinal;
+
+public class Atleta {
+	static int contarAtletas=0;
+	static String seleccion="España";
+	static double tiempoEquipo=0;
+	private int identificador;
+	private String nombre;
+	private double tiempo;
+	
+	public Atleta(String nombre, double tiempo) {
+		this.nombre = nombre;
+		this.tiempo = tiempo;
+		this.identificador+=contarAtletas;
+		contarAtletas++;
+	}
+
+	public int getIdentificador() {
+		return identificador;
+	}
+
+	public void setIdentificador(int identificador) {
+		this.identificador = identificador;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getTiempo() {
+		return tiempo;
+	}
+
+	public void setTiempo(double tiempo) {
+		this.tiempo = tiempo;
+	}
+
+	@Override
+	public String toString() {
+		return "Atleta [identificador=" + identificador + ", nombre=" + nombre + ", tiempo=" + tiempo + "]";
+	}
+	
+	
+	
+	
+}
---
+++ UD7.POO/src/RepasoFinal/Atleta_principal.java
@@ -0,0 +1,17 @@
+package RepasoFinal;
+
+public class Atleta_principal {
+
+	public static void main(String[] args) {
+		Atleta[] equipo = new Atleta[4];
+		equipo[0] = new Atleta("Antonio", 4.1);
+		equipo[1] = new Atleta("Antonio", 2.2);
+		equipo[2] = new Atleta("Antonio", 3.7);
+		equipo[3] = new Atleta("Antonio", 5.8);
+
+		for(Atleta mi:equipo) {
+			System.out.println(mi);
+		}
+	}
+
+}
