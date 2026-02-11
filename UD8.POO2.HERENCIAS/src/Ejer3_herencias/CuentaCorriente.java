bd9901f2c5782adeba69ce7c1f100b7477dcfed9
icem27 <icem27@educa.madrid.org>
2026-02-11 21:32:09 +0100

    nueva clase de CuentaCorriente

---
+++ UD8.POO2.HERENCIAS/src/Ejer3_herencias/CuentaCorriente.java
@@ -0,0 +1,12 @@
+package Ejer3_herencias;
+
+public class CuentaCorriente extends Cuenta{
+	private float descubierto=0;
+	
+	CuentaCorriente(float saldo, float tasa){
+		super(saldo, tasa);
+	}
+	
+	
+
+}
