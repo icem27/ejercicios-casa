210def458040f7bd81ca12c59288140e0a737b5b
icem27 <icem27@educa.madrid.org>
2026-02-11 20:54:32 +0100

    pequeñas mejores en Cuenta

--- UD8.POO2.HERENCIAS/src/Ejer3_herencias/Cuenta.java
+++ UD8.POO2.HERENCIAS/src/Ejer3_herencias/Cuenta.java
@@ -1,5 +1,7 @@
 package Ejer3_herencias;
 
+import java.util.Locale;
+
 public class Cuenta {
 	protected float saldo;
 	protected int num_ingresos;
@@ -17,6 +19,36 @@ public class Cuenta {
 	}
 	
 	void retirarDinero(float retiro) {
-		this.saldo-=retiro;
+		if(retiro>=saldo) {
+			this.saldo-=retiro;
+		} else {
+			System.out.println("No se puede realizar la operación, no tiene suficientes fondos");
+		}
+	}
+	
+	void calcInteres() {
+		this.saldo+=(saldo*(tasa/100))/12;
+	}
+	
+	void extractoMensual() {
+		this.saldo-=comision;
+		calcInteres();
+	}
+	
+	void imprimir() {
+		System.out.println("Su saldo actual es de: " + conDecimales(saldo)
+				+ "€ ya que el interés mensual es de: " + tasa
+				+ "% y la comisión ha sido de: " + conDecimales(comision));
+	}
+	
+	String conDecimales(float num) {
+		return String.format(Locale.GERMAN, "%,.2f", num);
+	}
+	
+	
+	public static void main(String[] args) {
+		Cuenta c1= new Cuenta(1000, 2);
+		c1.calcInteres();
+		c1.imprimir();
 	}
 }
