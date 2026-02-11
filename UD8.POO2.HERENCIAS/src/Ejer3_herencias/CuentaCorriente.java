4e4b63ed1622556a9242135f542b4b3e1ad95ed7
icem27 <icem27@educa.madrid.org>
2026-02-11 21:50:34 +0100

    queda pendiente todavia mejorarlo

--- UD8.POO2.HERENCIAS/src/Ejer3_herencias/CuentaCorriente.java
+++ UD8.POO2.HERENCIAS/src/Ejer3_herencias/CuentaCorriente.java
@@ -8,15 +8,19 @@ public class CuentaCorriente extends Cuenta{
 	}
 	
 	void retirarDinero(float retirada) {
+		float saltoAnterior=saldo;
+		saldo-=retirada;
 		if(saldo<=0) {
-			descubierto+=retirada;
+			descubierto+=(retirada-saltoAnterior);
+			saldo=0;
 		}
 	}
 	
 	@Override
 	void ingresoDinero(float ingreso) {
 		if(descubierto>=0) {
-			descubierto-=ingreso;
+			super.ingresoDinero((ingreso-descubierto));
+			descubierto=0;
 		} else {
 			super.ingresoDinero(ingreso);
 		}
@@ -31,6 +35,14 @@ public class CuentaCorriente extends Cuenta{
 	void imprimir() {
 		super.imprimir(); System.out.println("Has realizado " + (num_ingresos+num_retiradas) + " operaciones");
 		System.out.println("Descubierto: " + descubierto);
+	}
+	
+	public static void main(String[] args) {
+		CuentaCorriente c2=new CuentaCorriente(1000, 2);
+		c2.retirarDinero(1300);
+		c2.ingresoDinero(200);
+		c2.calcInteres();
+		c2.imprimir();
 	}
 
 }
