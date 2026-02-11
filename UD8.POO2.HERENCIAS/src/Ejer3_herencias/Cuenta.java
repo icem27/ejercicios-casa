9410e4cd272a90e88efb85d6edef07ac0f186d0e
icem27 <icem27@educa.madrid.org>
2026-02-11 10:45:11 +0100

    cambios importantes

--- UD8.POO2.HERENCIAS/src/Ejer3_herencias/Cuenta.java
+++ UD8.POO2.HERENCIAS/src/Ejer3_herencias/Cuenta.java
@@ -2,42 +2,22 @@ package Ejer3_herencias;
 
 public class Cuenta {
 	protected float saldo;
-	protected int num_ingresos = 0;
-	protected int num_retiradas = 0;
-	protected float tas;
-	protected float comision = 0;
+	protected int num_ingresos;
+	protected int num_retiradas;
+	protected float tasa;
+	protected float comision=0;
 	
-	Cuenta(){
-		
+	Cuenta(float saldo, float tasa){
+		this.saldo=saldo;
+		this.tasa=tasa;
 	}
 	
-	public Cuenta(float saldo, float tas) {
-		this.saldo = saldo;
-		this.tas = tas;
-	}
-	
-	void ingresarDinero(int ingreso) {
+	void ingresoDinero(float ingreso) {
 		this.saldo+=ingreso;
 	}
 	
-	void retiradaDinero(int retirada) {
-		this.saldo-=retirada;
-	}
-	
-	void calcularInteres() {
-		this.saldo+=(saldo*tas);
-	}
-	
-	void extractoMensual() {
-		this.saldo-=comision;
-		calcularInteres();
-	}
-	
-	void muestra() {
-		System.out.println("Saldo actual: " + saldo
-				+ " con " + num_ingresos + " número de ingresos y "
-				+ num_retiradas + " número de retiradas.\n" 
-				+ tas + "% es la tasa actual y la comisión " + comision);
+	void retirarDinero(float retiro) {
+		this.saldo-=retiro;
 	}
 	
 
