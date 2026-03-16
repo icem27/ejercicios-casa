8182b3c8440ea07a9ea0d2fc577a5b6249bf1e3b
manana <manana@10A1-alumno07>
2026-03-16 08:46:00 +0100

    Nuevo tema. UD 9 MANEJO DE EXCEPCIONES

---
+++ UD9.MANEJO.EXCEPCIONES/src/Ejercicios/Ejercicio1.java
@@ -0,0 +1,30 @@
+package Ejercicios;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		boolean repite = false;
+		do {
+			try {
+				System.out.println("Introduce el primer número: ");
+				int num1 = sc.nextInt();
+				System.out.println("Introduce el segundo número: ");
+				int num2 = sc.nextInt();
+				repite = true;
+				int div = num1 / num2;
+				System.out.println("División: " + div);
+
+			} catch (InputMismatchException e) {
+				System.out.println("Debes introduce números. ");
+				sc.nextLine();
+			} finally {
+				System.out.println("Adión. ");
+			}
+		} while (!repite);
+	}
+
+}
