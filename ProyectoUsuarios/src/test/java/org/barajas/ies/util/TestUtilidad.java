a7ea76a642256115ed6851cf50a95e11a3ae7ab4
icem27 <icem27@educa.madrid.org>
2026-03-24 13:22:03 +0100

    test

--- ProyectoUsuarios/src/test/java/org/barajas/ies/util/TestUtilidad.java
+++ ProyectoUsuarios/src/test/java/org/barajas/ies/util/TestUtilidad.java
@@ -1,7 +1,11 @@
 package org.barajas.ies.util;
 
+import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.barajas.ies.entities.Registro;
+import org.barajas.ies.entities.Usuario;
+import org.barajas.ies.services.Gestion;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -10,25 +14,59 @@ import org.junit.jupiter.api.Test;
 
 class TestUtilidad {
 
+	// Propiedad estática de la clase
+	private static Registro registro;
+
+	// Propiedad de la clase
+	private Usuario usuario;
+
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		// Inicializamos la propiedad "Registro"
+		// para tenerla disponible en los diferentes
+		// métodos de prueba
+		// Antes de la ejecución de cualquier método
+		registro = Registro.getInstance();
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		// Limpiamos el contenido de la propiedad "Registro"
+		// tras la ejecución de todos los métodos a probar
+		// en esta clase JUnit Test Case
+		registro = null;
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		// Inicializamos la propiedad "Usuario"
+		// para tenerla disponible en los diferentes
+		// métodos de prueba
+		usuario = new Usuario("Usuario", "usuario@mail.es", "Usu123");
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		// Aseguramos que después de la ejecución de
+		// cualquier método de prueba, el registro
+		// no tenga usuarios
+		for (int index = 0; index < registro.getUsuarios().length; index++) {
+			registro.setUsuario(null, index);
+		}
 	}
 
 	@Test
 	void testRegistroVacio() {
-		fail("Not yet implemented");
+		// fail("Not yet implemented");
+
+		// Comprobamos que inicialmente el registro no tiene usuarios
+		assertTrue(Utilidad.registroVacio(registro));
+
+		// Añadimos un usuario al registro, para comprobar cuando no está vacío
+		registro.setUsuario(new Usuario(), 0);
+
+		// Comprobamos que ahora el registro tiene algún usuario
+		assertFalse(Utilidad.registroVacio(registro));
 	}
 
 	@Test
@@ -53,7 +91,17 @@ class TestUtilidad {
 
 	@Test
 	void testPosicionUsuario() {
-		fail("Not yet implemented");
+		// fail("Not yet implemented");
+
+		// Si se inserto el usuario...
+		if (Gestion.addUser(registro, usuario)) {
+			// comprobaremos qeu la posición de nuestro "Usuario"
+			// es la primera
+			assertEquals(Utilidad.posicionUsuario(registro, usuario), 0);
+		} else {
+			// Sino se insertó dará fallo.
+			fail("No se insertó el usuario");
+		}
 	}
 
 	@Test
@@ -66,4 +114,4 @@ class TestUtilidad {
 		fail("Not yet implemented");
 	}
 
-}
+}    
