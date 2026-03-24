b23514b431c99b919664bed326240a64fde16824
icem27 <icem27@educa.madrid.org>
2026-03-24 12:57:34 +0100

    Cambios en TesGestion

--- ProyectoUsuarios/src/test/java/org/barajas/ies/services/TestGestion.java
+++ ProyectoUsuarios/src/test/java/org/barajas/ies/services/TestGestion.java
@@ -2,6 +2,8 @@ package org.barajas.ies.services;
 
 import static org.junit.Assert.*;
 
+import org.barajas.ies.entities.Registro;
+import org.barajas.ies.entities.Usuario;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -10,25 +12,57 @@ import org.junit.Test;
 
 public class TestGestion {
 
+	// Propiedad estática de la clase
+	private static Registro registro;
+
+	// Propiedad de la clase
+	private Usuario usuario;
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		// Inicializamos la propiedad "Registro"
+		// para tenerla disponible en los diferentes
+		// métodos de prueba
+		// Antes de la ejecución de cualquier método
+		registro = Registro.getInstance();
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
+		// Limpiamos el contenido de la propiedad "Registro"
+		// tras la ejecución de todos los métodos a probar
+		// en esta clase JUnit Test Case
+		registro = null;
 	}
 
 	@Before
 	public void setUp() throws Exception {
+		// Inicializamos la propiedad "Usuario"
+		// para tenerla disponible en los diferentes
+		// métodos de prueba
+		usuario = new Usuario("Usuario", "usuario@mail.es", "Usu123");
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		// Aseguramos que después de la ejecución de
+		// cualquier método de prueba, el registro
+		// no tenga usuarios
+		for (int index = 0; index < registro.getUsuarios().length; index++) {
+			registro.setUsuario(null, index);
+		}
 	}
 
 	@Test
 	public void testAddUser() {
-		fail("Not yet implemented");
+		// Si se inserto el usuario...
+		if (Gestion.addUser(registro, usuario)) {
+			// Comprobamos que el usuario insertado sea el de la propiedad
+			assertSame(usuario, registro.getUsuario(0));
+		} else {
+			// Sino se insertó dará fallo.
+			fail("No se insertó el usuario");
+		}
 	}
 
 	@Test
