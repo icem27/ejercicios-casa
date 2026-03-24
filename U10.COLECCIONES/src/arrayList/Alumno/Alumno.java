c550b308fa3c8a6bfa59c19be6fa08d7748b80f0
icem27 <icem27@educa.madrid.org>
2026-03-24 10:12:36 +0100

    Pequeños cambios en tema 10

---
+++ U10.COLECCIONES/src/arrayList/Alumno/Alumno.java
@@ -0,0 +1,42 @@
+package arrayList;
+
+public class Alumno implements Comparable<Alumno>{
+	private String nombre;
+	private double nota;
+	
+	public Alumno(String nombre, double nota) {
+		super();
+		this.nombre = nombre;
+		this.nota = nota;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getNota() {
+		return nota;
+	}
+
+	public void setNota(double nota) {
+		this.nota = nota;
+	}
+
+	@Override
+	public String toString() {
+		return "Alumno [nombre=" + nombre + ", nota=" + nota + "]";
+	}
+
+	@Override
+	public int compareTo(Alumno o) {
+		return Double.compare(this.nota, o.nota);
+	}
+	
+	
+	
+	
+}
